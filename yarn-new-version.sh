c89a6266fea30ec8bd99bb1cb2b30a96071076fa
Augusto Franzoia <ajfranzoia@gmail.com>
2017-05-18 18:54:03 -0300

    yarn new version

---
+++ yarn-new-version.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Moves last commit to a new branch and pushes to remote
+
+set -ex
+
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+if [ $CURRENT_BRANCH != "master" ]; then
+	echo "Current branch is not master! Aborting"
+	exit 1
+fi
+
+$NEW_VERSION=$1
+$DIST_DIR=dist
+
+# Generate new version using semver
+yarn version --new-version $NEW_VERSION
+
+# Push master update
+git push origin master
+
+# Delete created tag
+$CREATED_TAG=$(git describe --tags --abbrev=0)
+git tag -d $CREATED_TAG
+
+# Generate branch to create a new tag that includes the dist version
+$CREATED_TAG_TMP_BRANCH="$CREATED_TAG-tmp-branch"
+git checkout -b $CREATED_TAG_TMP_BRANCH
+
+# Build and add dist to git
+yarn run build
+git add -f $DIST_DIR
+git commit -m $CREATED_TAG
+git tag $CREATED_TAG
+git push origin $CREATED_TAG
+git checkout master
+git branch -D $CREATED_TAG_TMP_BRANCH
+
